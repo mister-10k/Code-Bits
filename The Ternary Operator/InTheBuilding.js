b86490ec7e31bbd37f362c5cdb5a8acc65d2c86f
mister-10k <kwabeohemeng@yahoo.com>
2020-07-26 22:01:49 -0400

    Ternary Operator code added

---
+++ The Ternary Operator/InTheBuilding.js
@@ -0,0 +1,5 @@
+var person1 = 'Kwabena';
+var person2 = 'Jack';
+var theChosenOne = person1.length >= person2.length ? person1 : person2;
+
+// theChoseOne - Kwabena
---
+++ The Ternary Operator/aintIt.js
@@ -0,0 +1,11 @@
+var person1 = 'Kwabena';
+var person2 = 'Jack';
+var theChosenOne;
+
+if (person1.length >= person2.length) {
+    theChosenOne = person1;
+} else {
+    theChosenOne = person2;
+}
+
+// theChoseOne - Kwabena
